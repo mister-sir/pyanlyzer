2e831c4f0ad66df3e32b5082cd2694597c3e534e
Brian Ulrich <brian@briandulrich.com>
2020-11-15 21:19:11 -0500

    Added some comments. PyDAQmx documentation sucks.

--- samples/basicTest.py
+++ samples/basicTest.py
@@ -1,18 +1,18 @@
 from PyDAQmx import *
 import numpy
 
-analog_input = Task()
+myTask = Task()
 read = int32()
 data = numpy.zeros((1000,), dtype=numpy.float64)
 
 # DAQmx Configure Code
-analog_input.CreateAIVoltageChan(b"Dev1/ai0","",DAQmx_Val_Cfg_Default,-10.0,10.0,DAQmx_Val_Volts,None)
-analog_input.CfgSampClkTiming("",10000.0,DAQmx_Val_Rising,DAQmx_Val_FiniteSamps,1000)
+myTask.CreateAIVoltageChan(b"Dev1/ai0","",DAQmx_Val_Cfg_Default,-10.0,10.0,DAQmx_Val_Volts,None) # Use Dev1/ai0, default config, -10 to 10 Volts, units of volts, no custom scaling
+myTask.CfgSampClkTiming("",10000.0,DAQmx_Val_Rising,DAQmx_Val_FiniteSamps,1000) # use the onboard clock, SR of 10000 Hz, rising edge (whatever, I don't care), Finite number of samples, acquire 1000 samples
 
 # DAQmx Start Code
-analog_input.StartTask()
+myTask.StartTask()
 
 # DAQmx Read Code
-analog_input.ReadAnalogF64(1000,10.0,DAQmx_Val_GroupByChannel,data,1000,byref(read),None)
+myTask.ReadAnalogF64(1000,10.0,DAQmx_Val_GroupByChannel,data,1000,byref(read),None)
 
 print("Acquired %d points",read.value)
